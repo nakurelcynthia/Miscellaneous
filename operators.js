1abf33e27b003ec008d3416a612ea6ff2c7fae99
Nakurel <nakurelcynthia@gmail.com>
2025-03-20 21:05:06 +0300

    practiced on js strings

---
+++ operators.js
@@ -0,0 +1,7 @@
+let a = "My name is Cynthia Nakurel";
+console.log(a.split());
+console.log(a.replace("name", "identity"));
+console.log(a.length);
+console.log(a.charAt(0));
+console.log(a.split(" ").reverse().join());
+console.log(a);
