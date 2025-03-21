7928ba53a625e7eb8a1c1effa7d5fe3158ece9fc
Nakurel <nakurelcynthia@gmail.com>
2025-03-21 17:06:16 +0300

    substrings as a method of extracting strings

--- operators.js
+++ operators.js
@@ -1,5 +1,5 @@
 let a = "My name is Cynthia Nakurel";
-console.log(a.split());
+console.log(a.split("name"));
 console.log(a.replace("name", "identity"));
 console.log(a.length);
 console.log(a.charAt(0));
@@ -9,3 +9,13 @@ console.log(a.at(12));
 console.log(a.at(-1));
 console.log(a.charCodeAt(0));
 console.log(a[0]);
+
+
+let t = "Hello";
+console.log(t.split("").reverse().join());
+console.log(t.charAt(0));
+console.log(t.slice(-1));
+console.log(t.indexOf("H"));
+console.log(a.slice(-7, ));
+console.log(a.slice(0));
+console.log(a.substring(0, ));
