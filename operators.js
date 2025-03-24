1a128799b8d61288ede959d709febbd24b889764
Nakurel <nakurelcynthia@gmail.com>
2025-03-24 11:38:36 +0300

    remove vowels in a string

--- operators.js
+++ operators.js
@@ -11,7 +11,7 @@ console.log(a.charCodeAt(0));
 console.log(a[0]);
 
 
-let t = "Hello";
+let t = "hello";
 console.log(t.split("").reverse().join());
 console.log(t.charAt(0));
 console.log(t.slice(-1));
@@ -19,3 +19,14 @@ console.log(t.indexOf("H"));
 console.log(a.slice(-7, ));
 console.log(a.slice(0));
 console.log(a.substring(0, ));
+console.log(t.at(-1));
+console.log(t.substring(0));
+console.log(t.toUpperCase());
+console.log(t.charAt(0).toUpperCase());
+let b = (a.slice(1).toLocaleUpperCase())
+
+
+let p = "Hello universe";
+console.log(p.split(" "));
+console.log(p.replace(/[aeiou]/gi, ''));
+console.log(p.match(/l/gi).length);
